ac27f5d030838a425a244dc96b6c42b3104856bf
Tim Hatch <timhatch@netflix.com>
2024-11-16 18:46:30 -0800

    Show minor edits to a strangely-formatted document

--- tests/test_box_yaml_mapping.py
+++ tests/test_box_yaml_mapping.py
@@ -11,3 +11,28 @@ def test_simple_mapping():
     assert stream["key"] == "val"
     # didn't make any edits, this should be fine
     assert stream.text == b"key: val\n"
+
+def test_terribly_complex_document():
+    stream = parse_str("""\
+key1: !tag {a: 1, b: 2}
+nulls:      { null, ~ }
+key2:
+ - seq1
+ - |-
+    some big item
+    here
+ - seq2
+blah: [ 4, 5   , 6]
+""")
+    stream["key2"][2] = "new item" # gets double quoted for now
+    assert stream.text.decode("utf-8") == """\
+key1: !tag {a: 1, b: 2}
+nulls:      { null, ~ }
+key2:
+ - seq1
+ - |-
+    some big item
+    here
+ - "new item"
+blah: [ 4, 5   , 6]
+"""
