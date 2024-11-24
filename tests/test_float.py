7816f95ae85a54bb5d370fc8d110c3db56eeb8fa
Tim Hatch <tim@timhatch.com>
2024-11-24 12:50:43 -0800

    Fix float roundtripping

--- tests/test_float.py
+++ tests/test_float.py
@@ -12,6 +12,7 @@ def test_smoke():
 
 def test_parse():
     assert parse_str("1.2")._root == 1.2
+    assert parse_str("1.5e20")._root == 1.5e20
     assert parse_str("+.inf")._root == float("inf")
     assert parse_str("-.inf")._root == float("-inf")
     # nan returns false for most operations
@@ -19,3 +20,11 @@ def test_parse():
     assert str(n) == "nan"
     assert not (n < 0)
     assert not (n >= 0)
+
+def test_scientific():
+    assert item(1.5e20).to_string() == "1.5e+20"
+
+def test_special_cases():
+    assert item(float("nan")).to_string() == ".nan"
+    assert item(float("inf")).to_string() == ".inf"
+    assert item(float("-inf")).to_string() == "-.inf"
